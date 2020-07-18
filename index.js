e6ad6aa9a560e24bbaf7aa8b499e13ebdc871f6c
Mohand Abdalrahman <mohand.abdalrahman@mondia.com>
2020-07-18 20:29:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const socket = require('socket.io')
+const app = express()
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT, () => console.log(`App listen on port ${PORT}`))
+
+// Socket setup
+const io = socket(server)
+io.on('connection', socket => {
+  console.log('Socket connect', socket.id);
+})
